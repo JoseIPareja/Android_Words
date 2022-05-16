e861db856479801ceb2bc40668f03fa82c30d9a7
josei <jose.ignacio97@gmail.com>
2022-05-16 17:22:36 -0300

    Added Intent to Detail Activity & Itent to Look up a word in a Browser

--- app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -15,6 +15,7 @@
  */
 package com.example.wordsapp
 
+import android.content.Intent
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
@@ -60,8 +61,19 @@ class LetterAdapter :
      * Replaces the content of an existing view with new data
      */
     override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
-        val item = list.get(position)
+        val item = list[position]
         holder.button.text = item.toString()
+        holder.button.setOnClickListener {
+            val context = holder.view.context
+            // Create an intent with a destination of DetailActivity
+            val intent = Intent(context, DetailActivity::class.java)
+            // Add the selected letter to the intent as extra data
+            // The text of Buttons are [CharSequence], a list of characters,
+            // so it must be explicitly converted into a [String].
+            intent.putExtra(DetailActivity.LETTER, holder.button.text.toString())
+            // Start an activity using the data and destination from the Intent.
+            context.startActivity(intent)
+        }
     }
 
     // Setup custom accessibility delegate to set the text read with
